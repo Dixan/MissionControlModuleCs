d7fab46b5c9d7bb14d213d972dd61e31e0a7cfd8
DickSan <d.v.kalsbeek@roc-teraa.nl>
2020-09-17 22:07:31 +0200

    Launch and set altitude button and eventhandler added

--- MissionControlModuleCs/Form1.Designer.cs
+++ MissionControlModuleCs/Form1.Designer.cs
@@ -28,35 +28,37 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.button1 = new System.Windows.Forms.Button();
-			this.button2 = new System.Windows.Forms.Button();
+			this.btnLaunch = new System.Windows.Forms.Button();
+			this.btnSetAltitude = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
-			// button1
+			// btnLaunch
 			// 
-			this.button1.Location = new System.Drawing.Point(164, 96);
-			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(75, 23);
-			this.button1.TabIndex = 0;
-			this.button1.Text = "button1";
-			this.button1.UseVisualStyleBackColor = true;
+			this.btnLaunch.Location = new System.Drawing.Point(164, 96);
+			this.btnLaunch.Name = "btnLaunch";
+			this.btnLaunch.Size = new System.Drawing.Size(75, 23);
+			this.btnLaunch.TabIndex = 0;
+			this.btnLaunch.Text = "Launch";
+			this.btnLaunch.UseVisualStyleBackColor = true;
+			this.btnLaunch.Click += new System.EventHandler(this.btnLaunch_Click);
 			// 
-			// button2
+			// btnSetAltitude
 			// 
-			this.button2.Location = new System.Drawing.Point(270, 160);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(75, 23);
-			this.button2.TabIndex = 1;
-			this.button2.Text = "button2";
-			this.button2.UseVisualStyleBackColor = true;
+			this.btnSetAltitude.Location = new System.Drawing.Point(270, 160);
+			this.btnSetAltitude.Name = "btnSetAltitude";
+			this.btnSetAltitude.Size = new System.Drawing.Size(75, 23);
+			this.btnSetAltitude.TabIndex = 1;
+			this.btnSetAltitude.Text = "Set altitude";
+			this.btnSetAltitude.UseVisualStyleBackColor = true;
+			this.btnSetAltitude.Click += new System.EventHandler(this.btnSetAltitude_Click);
 			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.button1);
+			this.Controls.Add(this.btnSetAltitude);
+			this.Controls.Add(this.btnLaunch);
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.ResumeLayout(false);
@@ -65,8 +67,8 @@
 
 		#endregion
 
-		private System.Windows.Forms.Button button1;
-		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button btnLaunch;
+		private System.Windows.Forms.Button btnSetAltitude;
 	}
 }
 
--- MissionControlModuleCs/Form1.cs
+++ MissionControlModuleCs/Form1.cs
@@ -16,5 +16,15 @@ namespace MissionControlModuleCs
 		{
 			InitializeComponent();
 		}
+
+		private void btnLaunch_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnSetAltitude_Click(object sender, EventArgs e)
+		{
+
+		}
 	}
 }
